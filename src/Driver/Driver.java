b05b18fc95e08c7ccb2c50de43899905e9a78216
Xavier Shelton <xayvair@gmail.com>
2019-08-19 06:23:11 -0400

    DrivingHistory: Added ways to get total hours and total driving history, now i just need to update it in the trips obj, and then run some test

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -6,23 +6,21 @@ import Trip.Trip;
 import java.util.ArrayList;
 
 /**
- *
  * <h1>Driver</h1>
  * The Driver Object, meant for creating instances of a driver.
  * Will set driver information for each driver
  *
- * @author  Xavier Shelton
+ * @author Xavier Shelton
  * @version 1.0
- * @since   2019-08-15
- *
+ * @since 2019-08-15
  */
 public class Driver {
     private String driverName = "";
     private int totalMiles;
-    private int avgSpeed ;
+    private int avgSpeed;
     private ArrayList<Trip> trips;
 
-    public Driver(String driverName){
+    public Driver(String driverName) {
         this.driverName = driverName;
         totalMiles = 0;
         avgSpeed = 0;
@@ -33,55 +31,64 @@ public class Driver {
         this.driverName = driverName;
     }
 
-    public String getDriverName(){
+    public String getDriverName() {
         return this.driverName;
     }
 
-    public void setTotalMiles(int totalMiles){
+    public void setTotalMiles(int totalMiles) {
         this.totalMiles = totalMiles;
     }
 
-    public int getTotalMiles(){
+    public int getTotalMiles() {
         return this.totalMiles;
     }
 
-    public void setTotalSpeed(int totalSpeed){
-        this.avgSpeed = totalSpeed;
-    }
 
-    public int getTotalSpeed(){
-        return this.avgSpeed;
-    }
-
-    public void setTrips(ArrayList<Trip> trips){
+    public void setTrips(ArrayList<Trip> trips) {
         this.trips = trips;
     }
 
-    public ArrayList<Trip> getTrips(){
+    public ArrayList<Trip> getTrips() {
         return this.trips;
     }
 
-    public int getAvgSpeed(){
-        return this.avgSpeed/this.totalMiles;
-    }
-    
-    private  int accumulatedHours(){
-        return 0;
+    public int getAvgSpeed() {
+        return this.avgSpeed / this.totalMiles;
     }
 
-    private int accumulatedMiles(){
-        return 0;
+    private int accumulatedHours(ArrayList<Trip> trips) {
+        int hours = 0;
+        for (Trip trip : trips) {
+            hours += trip.getTripDuration();
+        }
+        return hours;
     }
 
-    public  void addTrip(Trip trip){
+    private int accumulatedMiles(ArrayList<Trip> trips) {
+        int miles = 0;
+        for (Trip trip : trips) {
+            miles += trip.getMilesTraveled();
+        }
+        return miles;
+    }
+
+    public void addTrip(Trip trip) {
+
+        if (trip.getDriverName().equals(this.driverName)){
+            this.trips.add(trip);
+            this.totalMiles += trip.getMilesTraveled();
+            this.avgSpeed = getAvgSpeed();
+        }else{
+            throw new java.lang.RuntimeException("The following trip is not for this specific driver");
+        }
+    }
+
+    public void addTrips() {
 
     }
 
-    public  void addTrips(){
-
-    }
-
-    public  String generateDriverReport(){
+    public String generateDriverReport() {
         return "";
     }
+
 }
